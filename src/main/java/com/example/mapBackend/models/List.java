5d73baeceaa7e85810c108fd7f422ce656ed478b
lalebdi <loai.alebdi@gmail.com>
2020-08-29 17:22:41 -0400

    done with the models

--- src/main/java/com/example/mapBackend/models/List.java
+++ src/main/java/com/example/mapBackend/models/List.java
@@ -1,4 +1,36 @@
 package com.example.mapBackend.models;
 
+import sun.util.resources.Bundles;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="lists")
 public class List {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String list;
+
+    public List(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getList() {
+        return list;
+    }
+
+    public void setList(String list) {
+        this.list = list;
+    }
 }
