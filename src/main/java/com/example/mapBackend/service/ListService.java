949efd03be4430631bf1ba5c62880964907c8801
lalebdi <loai.alebdi@gmail.com>
2020-08-29 17:28:41 -0400

    done with the list interface

--- src/main/java/com/example/mapBackend/service/ListService.java
+++ src/main/java/com/example/mapBackend/service/ListService.java
@@ -1,4 +1,8 @@
 package com.example.mapBackend.service;
 
+import com.example.mapBackend.models.List;
+
 public interface ListService {
+    public Iterable<List> listLists();
+    public List createList(List list);
 }
