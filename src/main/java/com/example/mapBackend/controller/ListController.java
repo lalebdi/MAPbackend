6d26c7f4e73f3a68e0df2efc6acb8ee96e3cce35
lalebdi <loai.alebdi@gmail.com>
2020-08-29 17:37:51 -0400

    done with the controller

--- src/main/java/com/example/mapBackend/controller/ListController.java
+++ src/main/java/com/example/mapBackend/controller/ListController.java
@@ -1,4 +1,26 @@
 package com.example.mapBackend.controller;
 
+import com.example.mapBackend.models.List;
+import com.example.mapBackend.service.ListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/list")
 public class ListController {
+
+    @Autowired
+    private ListService listService;
+
+    @GetMapping
+    public Iterable<List> findAll() {
+        return listService.listLists();
+    }
+
+    @PostMapping
+    public List createList(@RequestBody List list){
+        return listService.createList(list);
+    }
+
 }
