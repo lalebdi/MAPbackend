37543ac41fb7170f8cc717d4d8032127c32f8d71
lalebdi <loai.alebdi@gmail.com>
2020-08-29 17:32:07 -0400

    done with the service part

--- src/main/java/com/example/mapBackend/service/ListServiceImpl.java
+++ src/main/java/com/example/mapBackend/service/ListServiceImpl.java
@@ -1,4 +1,23 @@
 package com.example.mapBackend.service;
 
-public class ListServiceImpl {
+import com.example.mapBackend.models.List;
+import com.example.mapBackend.repositories.ListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ListServiceImpl implements ListService{
+
+    @Autowired
+    private ListRepository listRepository;
+
+    @Override
+    public Iterable<List> listLists() {
+        return listRepository.findAll();
+    }
+
+    @Override
+    public List createList(List list) {
+        return listRepository.save(list);
+    }
 }
