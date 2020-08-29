f5e726ffce33ca53e3ccdbeff2b4eeb347e60e16
lalebdi <loai.alebdi@gmail.com>
2020-08-29 17:26:35 -0400

    done with the repo

--- src/main/java/com/example/mapBackend/repositories/ListRepository.java
+++ src/main/java/com/example/mapBackend/repositories/ListRepository.java
@@ -1,4 +1,12 @@
 package com.example.mapBackend.repositories;
 
-public interface ListRepository {
+import com.example.mapBackend.models.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ListRepository extends CrudRepository<List, Long> {
+
+    @Query("FROM List u WHERE u.list = ?1")
+    public List login(String list);
+
 }
